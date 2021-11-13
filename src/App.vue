c75c4c359656fda60b0f4d4f64fba188d27f3154
= <tmgast@gmail.com>
2021-11-13 22:55:30 +0900

    added basic elements

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="navbar-fixed">
+    <nav>
+      <div id="nav" class="nav-wrapper teal lighten-2">
+        <ul class="left">
+          <li :class="{'active': $route.path == '/'}">
+            <router-link tag="a" to="/">Home</router-link>
+          </li>
+          <li :class="{'active': $route.path == '/about'}">
+            <router-link tag="a" to="/about">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 #nav {
-  padding: 30px;
-
   a {
     font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
+}
+
+li.active a {
+  background-color: rgba(0,0,0, 0.5);
 }
 </style>
